bd492b89891f82114ce8db8e26e5fc767d3a7e80
sethjgore <sethjgore@gmail.com>
2014-03-21 13:07:18 -0500

    updated map size & contact panel content

--- partial/ribbon-contact-zz-d.php
+++ partial/ribbon-contact-zz-d.php
@@ -1,9 +1,26 @@
 <div class="block__contact zz_secondary zz_d">
   <div class="contact__box">
     <div class="contact__row">
-      <div class="contact__column">
+      <div class="contact__column map__neighbor">
         <p class="contact__message">CALL US TODAY!</p>
         <p class="contact__number">225.303.0125</p>
+        <p class="contact__email">
+          <a href="mailto:shredla@tigershreddingla.com">
+            OR SEND US AN EMAIL!
+          </a>
+        </p>
+      </div>
+      <div class="contact__column">
+       <?php
+
+          $maplink = get_field('google_maps', 'option');
+
+          if( !empty($maplink) ):
+          ?>
+          <div class="acf-map">
+            <div class="marker" data-lat="<?php echo $maplink['lat']; ?>" data-lng="<?php echo $maplink['lng']; ?>"></div>
+          </div>
+        <?php endif; ?>
       </div>
     </div>
   </div>
