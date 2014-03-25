b63d3ac1b0ac716fe12bfea1ae7e63402ce4a2fe
sethjgore <sethjgore@gmail.com>
2014-03-25 11:54:57 -0500

    fixed polyfill to show only on ios

--- grunt-tasks/options/uglify.js
+++ grunt-tasks/options/uglify.js
@@ -12,7 +12,12 @@ module.exports = {
       mangle: false
     },
     files: {
-      'js/script.js' : ['bower_components/jquery/dist/jquery.js', 'bower_components/owlcarousel/owl-carousel/owl.carousel.js' , 'js/parser.js', 'js/tokenizer.js', 'js/vminpoly.js', 'js/tigershredding.js']
+      'js/script.js' : ['bower_components/conditionizr/src/conditionizr.js', 'bower_components/jquery/dist/jquery.js', 'bower_components/owlcarousel/owl-carousel/owl.carousel.js' , 'js/tigershredding.js']
+    }
+  },
+  polyfills: {
+    files: {
+      'js/ios.js':['js/parser.js', 'js/tokenizer.js', 'js/vminpoly.js'],
     }
   }
 }
