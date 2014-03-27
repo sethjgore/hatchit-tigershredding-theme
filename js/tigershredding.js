c5aa0073684ebb3bb7b99058383022908e7de0a8
sethjgore <sethjgore@gmail.com>
2014-03-27 17:32:14 -0500

    removed social icons except for fb

--- js/tigershredding.js
+++ js/tigershredding.js
@@ -2,10 +2,6 @@ conditionizr.add('ios', [], function () {
   return /iP(ad|hone|od)/i.test(navigator.userAgent);
 });
 
-conditionizr.add('mac', [], function () {
-    return /mac/i.test(navigator.platform);
-});
-
 conditionizr.load('wp-content/themes/tigershredding/js/ios.js', ['ios']);
 
 $(document).ready(function() {
